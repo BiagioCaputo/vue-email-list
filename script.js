6f2f36e1ed7eea61e27ee74806f83d45cf0c45a6
BiagioCaputo <scebiagio98@gmail.com>
2024-01-30 09:43:42 +0100

    gli indirizzi adesso compaiono solo dopo essere stati generati

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ const app = createApp({
 
     }),
 
+    computed: {
+        //mi restituisce la lunghezza di destinations
+        lastElementIndex(){
+            return this.destinations.length - 1;
+        },
+       
+    },
+
     created() {
         //ciclo che per 10 volte avvia una funzione che salva nell'array vuoto "emails" un'email random fornita dall'endpoint
         for(let i=0;i<10;i++) {
